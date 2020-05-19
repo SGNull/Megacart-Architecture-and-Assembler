8a89e46d78e6c0d5a492afe3b558fd0cd32f3472
SGNull88 <60150561+SGNull88@users.noreply.github.com>
2020-05-19 14:44:16 -0400

    Settled the Integer to Binary issue by modifying TableEntry. Fleshed out the Assembler class more.

--- Assembler.cs
+++ Assembler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
+using Megacart_Assembler;
 
 public class Assembler
 {
@@ -13,32 +15,66 @@ public class Assembler
     public static string PathToUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
     public static string FileLocation = PathToUser + PathFromUser + FileName;
 
+    public static LookupTable LabelTable;
+    public static LookupTable ALUOperationsTable;
+    public static LookupTable InstructionsTable;
+    public static LookupTable ConditionsTable;
+    public static LookupTable InternalAddressesTable;
+
     public static void Main(String[] args)
     {
         if (File.Exists(FileLocation))
         {
-            ParseStringForLabels();
-            ParseString();
+            PopulateTables();
+            ParseFileForLabels();
+            ParseFile();
         }
 	}
 
-    public static void ParseStringForLabels()
+    public static void PopulateTables()
+    {
+
+    }
+
+    public static void ParseFileForLabels()
     {
         string[] fileLines = File.ReadAllLines(FileLocation);
+        ProgramCounter = 0;
 
         foreach (string line in fileLines)
         {
             string[] lineParts = Regex.Split(line, "[ ]+");
-            foreach (string mnemonic in lineParts)
+
+            if (lineParts[0] != "") //Line is not empty
             {
-                if(mnemonic != "")
-                    Debug.Print(mnemonic);
+                if (lineParts.Length > 3 && lineParts[3] != "")
+                    throw new FormatException("Line " + ProgramCounter + " too long: " + line);
+
+                TableEntry newTableEntry;
+
+                if (lineParts[0].ToLower() == "d6")
+                {
+                    newTableEntry = new TableEntry(lineParts[1], lineParts[2], true);
+                }
+                else if (lineParts[0].ToLower() == "d10")
+                {
+                    int intValue = Int32.Parse(lineParts[2]);
+                    string binValue = Convert.ToString(intValue, 2);
+                    newTableEntry = new TableEntry(lineParts[1], binValue, true);
+                }
+                else if (lineParts[0].ToLower() == ":")
+                {
+
+                }
+
+                ProgramCounter++;
             }
         }
     }
 
-    public static void ParseString()
+    public static void ParseFile()
     {
+        ProgramCounter = 0;
 
     }
 
--- TableEntry.cs
+++ TableEntry.cs
@@ -5,35 +5,79 @@ namespace Megacart_Assembler
 {
     class TableEntry
     {
+        //Big shoutout to https://stackoverflow.com/questions/1271562/binary-string-to-integer
+        private static readonly Regex Binary = new Regex("^[01]{1,32}$", RegexOptions.Compiled);
+
+        public bool? IsLine { get; set; }
+
         public string Keyword { get; set; }
 
-        private string value;
+        private string _value;
         public string Value
         {
-            get {return value;}
+            get {return _value;}
             set
             {
-                if (!binary.IsMatch(value))
+                if (!Binary.IsMatch(value))
+                    throw new FormatException("Not binary input value for Table Entry: Keyword-" + Keyword);
+
+                if (value.Length > 6)
+                    throw new OverflowException("Input value too large for Table Entry: Keyword-" + Keyword);
+
+                int zeros;
+                if (IsLine == null)
                 {
-                    Console.WriteLine("Improper input value for Table Entry: Keyword-" + Keyword);
-                    throw new FormatException();
+                    zeros = 0;
                 }
-                else if (value.Length > 6)
+                else if ((bool) IsLine)
                 {
-                    Console.WriteLine("Input value too large for Table Entry: Keyword-" + Keyword);
-                    throw new OverflowException();
+                    zeros = 6 - value.Length;
                 }
                 else
                 {
-                    this.value = value;
+                    if (value.Length > 3)
+                        throw new OverflowException("Input value too large for tribble (3-bit) Table Entry: Keyword-" + Keyword);
+                    zeros = 3 - value.Length;
                 }
+
+                string newValue = value;
+                while (zeros > 0)
+                {
+                    newValue = 0 + newValue;
+                    zeros--;
+                }
+
+                _value = newValue;
             }
         }
-
-        //Big shoutout to https://stackoverflow.com/questions/1271562/binary-string-to-integer
-        private static readonly Regex binary = new Regex("^[01]{1,32}$", RegexOptions.Compiled);
         public TableEntry(string keyword, string value)
         {
+            this.IsLine = null;
+            this.Keyword = keyword;
+            this.Value = value;
+        }
+
+        public TableEntry(string keyword, int value)
+        {
+            string valueString = Convert.ToString(value, 2);
+
+            this.IsLine = null;
+            this.Keyword = keyword;
+            this.Value = valueString;
+        }
+
+        public TableEntry(string keyword, int value, bool? isLine)
+        {
+            string valueString = Convert.ToString(value, 2);
+
+            this.IsLine = isLine;
+            this.Keyword = keyword;
+            this.Value = valueString;
+        }
+
+        public TableEntry(string keyword, string value, bool? isLine)
+        {
+            this.IsLine = isLine;
             this.Keyword = keyword;
             this.Value = value;
         }
